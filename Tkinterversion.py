edf5fd58cd6d454d723d5d2ff2cee20386ae2c6b
gabriel brian <gabi.silver1998@gmail.com>
2021-02-08 23:55:31 +0200

    Added coordinate extraction

--- Tkinterversion.py
+++ Tkinterversion.py
@@ -2,7 +2,8 @@ import tkinter as tk
 import csv
 from math import acos, degrees
 import math
-
+import os
+from exif import Image
 
 class Lens:
     def __init__(self,mm , x_fov, y_fov):
@@ -34,11 +35,16 @@ CamD850 = Camera(8256,5504)
 CamD5 = Camera(5588,3712)
 Sony_Ar7= Camera(9504,6336)
 
+
+
+
 root = tk.Tk()
 
 def Get():
 	text_box.delete(1.0, "end-1c")
+	
 	Currentcam = cam_var.get()
+	
 	if Currentcam == "D5":
 		Currentcam = CamD5.x_res
 	elif Currentcam == "D850":
@@ -138,11 +144,39 @@ def Get():
 		writer.writerow([Avghl])
 		writer.writerow(["------------------------------------------------------- "])
 
+
+def getLoc():
+	text_box2.delete(1.0, "end-1c")
+	Dir = Picfolder_entry.get()
+	text_box2.insert('1.0',"-------coordinates-------")
+	def dms_to_dd(gps_coords, gps_coords_ref):
+	    d, m, s =  gps_coords
+	    dd = d + m / 60 + s / 3600
+	    if gps_coords_ref.upper() in ('S', 'W'):
+	        return -dd
+	    elif gps_coords_ref.upper() in ('N', 'E'):
+	        return dd
+	    else:
+	        raise RuntimeError('Incorrect gps_coords_ref {}'.format(gps_coords_ref))
+	        
+	for pic in os.listdir(Dir):
+	    fullpath = os.path.join(Dir,pic)       
+	    with open(fullpath, 'rb') as image_file:
+	        my_image = Image(image_file)
+	                
+	        latitude_final = dms_to_dd(my_image.gps_latitude, my_image.gps_latitude_ref)
+	        longitude_final = dms_to_dd(my_image.gps_longitude, my_image.gps_longitude_ref)
+	                
+	        complete = latitude_final,longitude_final
+	        text_box2.insert('end','\n')
+	        text_box2.insert('end',complete)
+		
+
 root.title('PhotoCalculator')
-root.geometry("400x400") 
+root.geometry("800x450") 
 root.resizable(0, 0)
 
-background_image = tk.PhotoImage(file='plane.png')
+background_image = tk.PhotoImage(file='Plane.png')
 background_label = tk.Label(root, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 
@@ -175,8 +209,19 @@ LensMenu.place(x=40,y= 50)
 Calculate = tk.Button(root, text = "Calculate",font=("Courier", 14,"bold"),bg="#F2DDEC",activebackground = "grey", command = Get)
 Calculate.place(width="100px",x = 130 , y = 100)
 
-text_box = tk.Text(root, width = 40, height = 14)
+text_box = tk.Text(root, width = 40, height = 16,borderwidth=3)
 text_box.place(x=40,y=150)
+
+Picfolder = tk.Label(root,font=("Courier", 8),bg="#C5F0CE",text="Folder path :",bd = 2)
+Picfolder.place(x=450,y=50)
+Picfolder_entry = tk.Entry(root,bd = 2)
+Picfolder_entry.place(x=550,y=50,width=200)
+
+Get_location = tk.Button(root, text = "Get coordinates",font=("Courier", 12,"bold"),bg="#F2DDEC",activebackground = "grey",command = getLoc)
+Get_location.place(width="140px",x = 520 , y = 100)
+
+text_box2 = tk.Text(root, width = 40, height = 16,borderwidth=3)
+text_box2.place(x=450,y=150)
 
 root.mainloop()
 
